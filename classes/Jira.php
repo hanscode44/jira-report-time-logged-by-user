0c5d2a09b9499ce37a85873cb042941c3f8df368
Hans Loos <hansloos.g1@gmail.com>
2017-07-08 11:23:36 +0200

    fixing some minor php issues

--- classes/Jira.php
+++ classes/Jira.php
@@ -5,119 +5,122 @@ use Curl\Curl;
 /**
  * Class jira
  */
-class Jira
-{
+class Jira {
 
-    /**
-     * @param $period string
-     *
-     * @return mixed
-     */
-    function getData($period, $startDate = null, $endDate = null)
-    {
-        global $cfg;
+	/**
+	 * @param $period string
+	 *
+	 * @return mixed
+	 */
+	function getData( $period, $startDate = null, $endDate = null ) {
+		global $cfg;
 
-        if ($period == 'today') {
-            $startDate = new \DateTime();
-            $endDate = new \DateTime('tomorrow');
-        } else if ($period == 'yesterday') {
-            $startDate = new \DateTime('yesterday');
-            $endDate = new \DateTime();
-        } else if ($period == 'week') {
-            $startDate = new \DateTime('monday this week');
-            $endDate = new \DateTime('saturday this week');
-        } else if($period = 'period'){
-            $startDate = new \DateTime($startDate);
-            $endDate = new \DateTime($endDate);
-        }
+		if ( $period == 'today' ) {
+			$startDate = new \DateTime();
+			$endDate   = new \DateTime( 'tomorrow' );
+		} else if ( $period == 'yesterday' ) {
+			$startDate = new \DateTime( 'yesterday' );
+			$endDate   = new \DateTime();
+		} else if ( $period == 'week' ) {
+			$startDate = new \DateTime( 'monday this week' );
+			$endDate   = new \DateTime( 'saturday this week' );
+		} else if ( $period = 'period' ) {
+			$startDate = new \DateTime( $startDate );
+			$endDate   = new \DateTime( $endDate );
+		}
 
-        $fromDate = $startDate->format('Y-m-d');
-        $toDate = $endDate->format('Y-m-d');
-        $periodLog = [];
+		$fromDate  = $startDate->format( 'Y-m-d' );
+		$toDate    = $endDate->format( 'Y-m-d' );
+		$periodLog = [];
 
-        $curl = new Curl();
-        $curl->setOpt(CURLOPT_USERPWD, $cfg['jira_user_name'] . ':' . $cfg['jira_user_password']);
-        $curl->setOpt(CURLOPT_FOLLOWLOCATION, true);
-        $curl->setOpt(CURLOPT_RETURNTRANSFER, true);
+		$curl = new Curl();
+		$curl->setOpt( CURLOPT_USERPWD, $cfg['jira_user_name'] . ':' . $cfg['jira_user_password'] );
+		$curl->setOpt( CURLOPT_FOLLOWLOCATION, true );
+		$curl->setOpt( CURLOPT_RETURNTRANSFER, true );
 
-        $curl->setOpt(
-            CURLOPT_URL,
-            $cfg['jira_host_address'] . "/rest/api/2/search?startIndex=0&jql=" .
-            "worklogAuthor=" . $cfg['jira_user_name'] . "+and+updated+%3E+$fromDate+" .
-            "and+timespent+%3E+0&fields=key,summary&maxResults=100"
-        );
+		$curl->setOpt(
+			CURLOPT_URL,
+			$cfg['jira_host_address'] . "/rest/api/2/search?startIndex=0&jql=" .
+			"worklogAuthor=" . $cfg['jira_user_name'] . "+and+updated+%3E+$fromDate+" .
+			"and+timespent+%3E+0&fields=key,summary&maxResults=100"
+		);
 
-        $curl->exec();
-        $returnData = json_decode(json_encode($curl->response), true);
+		$curl->exec();
+		$returnData = json_decode( json_encode( $curl->response ), true );
 
-        foreach ($returnData['issues'] as $issue) {
+		if ( $returnData ) {
+			foreach ( $returnData['issues'] as $issue ) {
 
-            $key = $issue['key'];
-            $title = $issue['fields']['summary']; // needed for future addition
+				$key   = $issue['key'];
+				$title = $issue['fields']['summary']; // needed for future addition
 
-            $curl->setOpt(CURLOPT_URL, $cfg['jira_host_address'] . "/rest/api/2/issue/$key/worklog");
-            $curl->exec();
+				$curl->setOpt( CURLOPT_URL, $cfg['jira_host_address'] . "/rest/api/2/issue/$key/worklog" );
+				$curl->exec();
 
-            $worklog = json_decode(json_encode($curl->response), true);
+				$worklog = json_decode( json_encode( $curl->response ), true );
 
-            $loopcounter = 0;
+				$loopcounter = 0;
 
-            foreach ($worklog['worklogs'] as $entry) {
+				foreach ( $worklog['worklogs'] as $entry ) {
 
-                if ($entry['author']['name'] == $cfg['jira_user_name']) {
-                    $shortDate = substr($entry['started'], 0, 10);
-                    $startDate = new \DateTime($entry['started']);
+					if ( $entry['author']['name'] == $cfg['jira_user_name'] ) {
+						$shortDate = substr( $entry['started'], 0, 10 );
+						$startDate = new \DateTime( $entry['started'] );
 
-                    if ($shortDate >= $fromDate && $shortDate < $toDate) {
-	                    $periodLog[$key]['description'] = $title;
-	                    $periodLog[$key]['timespent'][$startDate->format('Y-m-d')][$loopcounter]['time'] = $entry['timeSpentSeconds'] / 60;
-	                    $periodLog[$key]['timespent'][$startDate->format('Y-m-d')][$loopcounter]['comment'] = $entry['comment'];
-	                    $loopcounter++;
-                    }
-                }
-            }
-        }
+						if ( $shortDate >= $fromDate && $shortDate < $toDate ) {
+							$periodLog[ $key ]['description']                                                           =
+								$title;
+							$periodLog[ $key ]['timespent'][ $startDate->format( 'Y-m-d' ) ][ $loopcounter ]['time']    =
+								$entry['timeSpentSeconds'] / 60;
+							$periodLog[ $key ]['timespent'][ $startDate->format( 'Y-m-d' ) ][ $loopcounter ]['comment'] =
+								$entry['comment'];
+							$loopcounter ++;
+						}
+					}
+				}
+			}
+		}
+		$curl->close();
 
-        $curl->close();
-        return $periodLog;
+		return $periodLog;
 
-    }
+	}
 
-    /**
-     * @param $data array
-     *
-     * @return array|bool
-     */
-    function buildRowFromData($data)
-    {
-        global $error;
+	/**
+	 * @param $data array
+	 *
+	 * @return array|bool
+	 */
+	function buildRowFromData( $data ) {
+		global $error;
 
-        if (empty($data)) {
-            $error = 'Error: Request did not return any results, check login information or project key';
+		if ( empty( $data ) ) {
+			$error = 'Error: Request did not return any results, check login information or project key';
 
-            return false;
-        }
+			return false;
+		}
 
-        $arr = [];
-        foreach ($data as $i => $issue) {
+		$arr = [];
+		foreach ( $data as $i => $issue ) {
 
-            $timespent = 0;
+			$timespent = 0;
 
-            foreach ($issue['timespent'] as $d => $ts) {
+			foreach ( $issue['timespent'] as $d => $ts ) {
 
-            	$entryCounter = 0;
-                foreach ($ts as $entry) {
-                    $timespent = $timespent + $entry['time'];
-                    $arr[$i]['entry'][$d][$entryCounter]['minutes'] = $entry['time'];
-	                $arr[$i]['entry'][$d][$entryCounter]['description'] = $entry['comment'];
-	                $entryCounter++;
-                }
-            }
+				$entryCounter = 0;
+				foreach ( $ts as $entry ) {
+					$timespent                                                = $timespent + $entry['time'];
+					$arr[ $i ]['entry'][ $d ][ $entryCounter ]['minutes']     = $entry['time'];
+					$arr[ $i ]['entry'][ $d ][ $entryCounter ]['description'] = $entry['comment'];
+					$entryCounter ++;
+				}
+			}
 
-            $arr[$i]['description'] = $issue['description'];
-            $arr[$i]['total_time_spent_minutes'] = $timespent;
-            $arr[$i]['total_time_spent_hours'] = $timespent / 60;
-        }
-        return $arr;
-    }
+			$arr[ $i ]['description']              = $issue['description'];
+			$arr[ $i ]['total_time_spent_minutes'] = $timespent;
+			$arr[ $i ]['total_time_spent_hours']   = $timespent / 60;
+		}
+
+		return $arr;
+	}
 }
